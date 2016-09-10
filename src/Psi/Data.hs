7f743f70ca56a876491110924c0039b82860ff67
Sharif Olorin <sio@tesser.org>
2016-09-10 06:34:13 +0000

    Export more basic data types

--- src/Psi/Data.hs
+++ src/Psi/Data.hs
@@ -3,16 +3,31 @@
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 
 module Psi.Data (
-    ByteString
-  , NonEmpty(..)
-  , Text
-  , (<>)
-  , module X
+    module X
   ) where
 
-import          Data.ByteString (ByteString)
-import          Data.List.NonEmpty (NonEmpty(..))
-import          Data.Semigroup ((<>))
-import          Data.Text (Text)
-import          Data.Word as X (Word, Word8, Word16, Word32, Word64)
+import           Data.Bifunctor as X (Bifunctor(..))
+import           Data.Bool as X
+import           Data.ByteString as X (ByteString)
+import           Data.Char as X (Char)
+import           Data.Either as X
+import           Data.Eq as X
+import           Data.Functor as X (Functor(..))
+import           Data.Functor as X (($>), (<$>), void)
+import           Data.Int as X
+import           Data.List.NonEmpty (NonEmpty(..))
+import           Data.Maybe as X hiding (fromJust)
+import           Data.Ord as X
+import           Data.Semigroup as X ((<>))
+import           Data.Text as X (Text)
+import           Data.Traversable as X
+import           Data.Tuple as X
+import           Data.Word as X (Word, Word8, Word16, Word32, Word64)
 
+import           GHC.Num as X
+import           GHC.Real as X
+import           GHC.Float as X
+
+import           Prelude as X (Enum, Bounded)
+import           Prelude as X (($!))
+import           Prelude as X (minBound, maxBound, seq)
